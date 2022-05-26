84049d1825ffd1e78697faf3f8de1685423c1a9f
kluaynum951 <kluaynum951@gmail.com>
2022-05-26 22:48:00 +0700

    Set up bootstrapVue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
     <router-view/>
   </div>
 </template>
@@ -15,6 +11,8 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-color: #E4E6EF;
+  height: 100vh;
 }
 
 nav {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col cols="8">
+          <b-card title="Card title" sub-title="Card subtitle">
+            <b-card-text>
+              Some quick example text to build on the <em>card title</em> and make up the bulk of the card's
+              content.
+            </b-card-text>
+
+            <b-card-text>A second paragraph of text in the card.</b-card-text>
+
+            <a href="#" class="card-link">Card link</a>
+            <b-link href="#" class="card-link">Another link</b-link>
+          </b-card>
+        </b-col>
+        <b-col cols="4">
+          <b-card title="Card title" sub-title="Card subtitle">
+            <b-card-text>
+              Some quick example text to build on the <em>card title</em> and make up the bulk of the card's
+              content.
+            </b-card-text>
+
+            <b-card-text>A second paragraph of text in the card.</b-card-text>
+
+            <a href="#" class="card-link">Card link</a>
+            <b-link href="#" class="card-link">Another link</b-link>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  name: 'HomeView'
 }
 </script>
+
+<style lang="scss" scoped>
+
+</style>
